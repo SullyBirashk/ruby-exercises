0ff32b27a6ca58a5e0a305a40f8c8391f55c0043
Sully Birashk <sullybirashk@Sullys-MacBook-Pro.local>
2024-02-15 19:33:48 -0800

    Refactor: Create Attr Reader

--- mythical-creatures/lib/vampire.rb
+++ mythical-creatures/lib/vampire.rb
@@ -1,21 +1,10 @@
 class Vampire
+    attr_reader :name, :pet, :thirsty
 
     def initialize(name, pet = 'bat')
         @name = name 
         @pet = pet
         @thirsty = true
-    end
-
-    def name
-        @name
-    end
-
-    def pet
-        @pet
-    end
-
-    def thirsty
-        @thirsty
     end
 
     def drink 
